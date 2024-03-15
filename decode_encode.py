002ec9862f4ddb15f8dfde81780f28ec9c9c5c79
GiftGiwa <gdg2131@columbia.edu>
2024-03-14 20:20:37 -0400

    an (failed) attempt at extending CCLocalLevel retrieval functionality to MacOS

--- decode_encode.py
+++ decode_encode.py
@@ -4,7 +4,7 @@ import base64
 import zlib
 import gzip
 from os.path import expanduser
-
+from Crypto.Cipher import AES
 
 class CCLocalLevels:
 
@@ -34,15 +34,15 @@ class CCLocalLevels:
         home = ""
         current_platform = platform.platform() # get operating system name
 
+        # print(current_platform) # for testing purposes
+
         # use these to include either a forward or a backwards slash in the directory
         op_sys_index = 0 
         slashes = ["/", "\\"]
 
         if "Windows" in current_platform: # windows
-            # print("in Windows")
             home = expanduser("~")
             home += "\\AppData\\Local\\GeometryDash"
-            local_levels_query = 'GeometryDash\\CCLocalLevels.dat'
             op_sys_index = 1
         elif "microsoft-standard-WSL" in current_platform: # WSL
             non_users = ['All Users', 'Default', 'Default User', 'desktop.ini', 'Public']
@@ -53,40 +53,83 @@ class CCLocalLevels:
                     home = "/mnt/c/Users/" + user + "/AppData/Local/GeometryDash"
                     break
             op_sys_index = 0
+        elif "macOS" in current_platform: # MacOS
+            home = expanduser("~")
+            home += "/Library/Application Support/GeometryDash"
+            op_sys_index = 0
 
         CCLocalLevels_path = ""
         for root, dirs, files in os.walk(home):
+
+            print(root)
+            # print("Files in this directory: " + files)
             for file in files: 
                 if (file.endswith("CCLocalLevels.dat") and root.endswith("GeometryDash")):
                     # forward slash for Windows, backwards for WSL
                     CCLocalLevels_path = root + slashes[op_sys_index] + file
                     break
         
+        # print("CCLocalLevels path: " + CCLocalLevels_path)
         f = open(CCLocalLevels_path, "r")
-        self.CCLocalLevels_encrypted = (f.read()) # encrypted version of CCLocalLevels
+        self.CCLocalLevels_encrypted = f.read() # encrypted version of CCLocalLevels
         return
 
     # decrypt CCLocalLevels
     # source: https://wyliemaster.github.io/gddocs/#/topics/localfiles_encrypt_decrypt?id=windows-1
-    def decrypt(self, s: str):
+    def decrypt(self, s: str) -> str:
 
-        def xor(string: str, key: int) -> str:
-            return ("").join(chr(ord(char) ^ key) for char in string)
+        if "macOS" in platform.platform(): # MacOS decryption scheme
+            def remove_pad(data: bytes) -> bytes:
+                last = data[-1]
+                if last < 16:
+                    data = data[:-last]
+                return data
+            
+            KEY = (  # python will automatically concatenate two parts
+                    b"\x69\x70\x75\x39\x54\x55\x76\x35\x34\x79\x76\x5d\x69\x73\x46\x4d"
+                    b"\x68\x35\x40\x3b\x74\x2e\x35\x77\x33\x34\x45\x32\x52\x79\x40\x7b"
+            )
+
+            cipher = AES.new(KEY, AES.MODE_ECB)
+            return remove_pad(cipher.decrypt(s)).decode()
         
-        base64_decoded = base64.urlsafe_b64decode(xor(s, key=11).encode())
-        decompressed = gzip.decompress(base64_decoded)
-        return decompressed.decode()
+        else : # Windows decryption scheme
+
+            def xor(string: str, key: int) -> str:
+                return ("").join(chr(ord(char) ^ key) for char in string)
+            
+            base64_decoded = base64.urlsafe_b64decode(xor(s, key=11).encode())
+            decompressed = gzip.decompress(base64_decoded)
+            return decompressed.decode()
 
     # encrypt CCLocalLevels
     # source: https://wyliemaster.github.io/gddocs/#/topics/localfiles_encrypt_decrypt?id=windows-1
     def encrypt(self, s: str):
 
-        def xor(string: str, key: int) -> str:
-            return ("").join(chr(ord(char) ^ key) for char in string)
+        if "macOS" in platform.platform(): # MacOS encryption scheme
+            def add_pad(data: bytes) -> bytes:
+                len_r = len(data) % 16
+                if len_r:
+                    to_add = 16 - len_r
+                    data += to_add.to_bytes(1, "little") * to_add
+                return data
+            
+            KEY = (  # python will automatically concatenate two parts
+                    b"\x69\x70\x75\x39\x54\x55\x76\x35\x34\x79\x76\x5d\x69\x73\x46\x4d"
+                    b"\x68\x35\x40\x3b\x74\x2e\x35\x77\x33\x34\x45\x32\x52\x79\x40\x7b"
+            )
+            
+            cipher = AES.new(KEY, AES.MODE_ECB)
+            return cipher.encrypt(add_pad(s.encode()))
 
-        gzipped = gzip.compress(s.encode())
-        base64_encoded = base64.urlsafe_b64encode(gzipped)
-        return xor(base64_encoded.decode(), key=11)
+        else: # Window encryption scheme
+
+            def xor(string: str, key: int) -> str:
+                return ("").join(chr(ord(char) ^ key) for char in string)
+
+            gzipped = gzip.compress(s.encode())
+            base64_encoded = base64.urlsafe_b64encode(gzipped)
+            return xor(base64_encoded.decode(), key=11)
 
 
 # if __name__ == '__main__':
--- level_parser.py
+++ level_parser.py
@@ -4,7 +4,7 @@ import zlib
 import gzip
 
 # store info for level (i.e. "repository")
-class Levels:
+class LevelParser:
     def __init__(self, cc_local_levels: CCLocalLevels):
         self.level_string = cc_local_levels.CCLocalLevels_decrypted
         self.levels = []
@@ -24,11 +24,11 @@ class Levels:
          
 
 if __name__ == '__main__':
-    levels = Levels(CCLocalLevels())
+    levels = LevelParser(CCLocalLevels())
     # just some test code
     level_string = "H4sIAAAAAAAAC6WT0W3DMAxEF1IAHknJCvqVGTIAB8gKHb6iTkgTwG0c5MM8m0c-kQJ8u1ovCJfQgNaw0FoDoCiFSY8TogVEJLZAoGboIdED34iJED2GwOeI8y4ia9hwCKKR_Xugt65kfH_O8P8WmhA0vFqo_rnQm6D2CnT0ivfWKrcLrEhKpTSKlxH5vs2oy-cXesrVzvRmJGcaF5-RLoRFrFKWKSt0K8hyoYCiU5QoJcXoGT3jaE6YE-aVnlGcwrmNB-VPlnKegjU-YbrmBKce8pVnarFxN4M56HU8K1n7Y7KMrrziZXqbpjy3uD1lH3vE7n7Hsls5ocuv23bcO-MHh6d3MUAEAAA="
 
-    print(levels.decode_level(level_string))
+    # print(levels.decode_level(level_string))
     
 
 
